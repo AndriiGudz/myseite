eeb6eff8bb8763c6254169bbc35957e40eb2cf7b
Andrii Gudz <andrey.gudz@gmail.com>
2024-09-25 18:40:37 +0200

    update error, remove useCallback

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useCallback } from 'react';
-import BlinkingText from '../../components/BlinkingText/BlinkingText';
-import Button from '../../components/Button/Button';
+import { useEffect, useRef } from 'react'
+import BlinkingText from '../../components/BlinkingText/BlinkingText'
+import Button from '../../components/Button/Button'
 import {
   BtnContact,
   ContactBlock,
@@ -14,53 +14,53 @@ import {
   TitleHome1,
   TitleHome2,
   TitleSmall,
-} from './styles';
-import myPhoto from '../../assets/my-foto-1.webp';
-import { useTranslation } from 'react-i18next'; // Импортируем хук для перевода
+} from './styles'
+import myPhoto from '../../assets/my-foto-1.webp'
+import { useTranslation } from 'react-i18next' // Импортируем хук для перевода
 
 function Home() {
-  const { t } = useTranslation(); // Используем хук для доступа к переводам
+  const { t } = useTranslation() // Используем хук для доступа к переводам
 
   // Используем useCallback, чтобы гарантировать стабильность функции и избежать ESLint ошибок
-  const descriptionRef = useRef<HTMLDivElement | null>(null); 
-  const contactRef = useRef<HTMLParagraphElement | null>(null); 
+  const descriptionRef = useRef<HTMLDivElement | null>(null)
+  const contactRef = useRef<HTMLParagraphElement | null>(null)
 
   useEffect(() => {
-    const currentDescriptionRef = descriptionRef.current;
-    const currentContactRef = contactRef.current;
+    const currentDescriptionRef = descriptionRef.current
+    const currentContactRef = contactRef.current
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('visible'); // Добавляем класс при видимости
+          entry.target.classList.add('visible') // Добавляем класс при видимости
         } else {
-          entry.target.classList.remove('visible'); // Убираем класс, если элемент не виден
+          entry.target.classList.remove('visible') // Убираем класс, если элемент не виден
         }
       },
       {
         threshold: 0.1, // Сработает, когда 10% элемента будет видимым
       }
-    );
+    )
 
     if (currentDescriptionRef) {
-      observer.observe(currentDescriptionRef);
+      observer.observe(currentDescriptionRef)
     }
 
     if (currentContactRef) {
-      observer.observe(currentContactRef);
+      observer.observe(currentContactRef)
     }
 
     // Функция очистки при размонтировании компонента
     return () => {
       if (currentDescriptionRef) {
-        observer.unobserve(currentDescriptionRef);
+        observer.unobserve(currentDescriptionRef)
       }
 
       if (currentContactRef) {
-        observer.unobserve(currentContactRef);
+        observer.unobserve(currentContactRef)
       }
-    };
-  }, []);
+    }
+  }, [])
 
   return (
     <PageBox>
@@ -96,7 +96,7 @@ function Home() {
         </BtnContact>
       </ContactBlock>
     </PageBox>
-  );
+  )
 }
 
-export default Home;
+export default Home
